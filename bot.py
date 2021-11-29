5d1a92aca146f1c8a0f4aafacb625d70eb325814
jinicha <jinichaa@gmail.com>
2021-11-29 02:21:21 -0800

    Previous code not buying items properly, fix issue and refactor code after reading solution code

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-import threading
 import config
 
 chrome_driver_path = config.chrome_driver_path
@@ -12,29 +11,28 @@ browser = webdriver.Chrome(service=service)
 
 browser.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = browser.find_element(By.ID, "cookie")
-money = browser.find_element(By.ID, "money")
 products = browser.find_elements(By.CSS_SELECTOR, "#rightPanel b")[:-1]
 all_products = [p.text.split("-")[0].strip() for p in products]
-prices = [p.text.split("-")[1].strip() for p in products]
-grayed_products = browser.find_elements(By.CSS_SELECTOR, ".grayed b")
-not_available_products = [p.text.split("-")[0].strip() for p in grayed_products]
 
+five_sec = time.time() + 5
+five_min = time.time() + 60*2
 
-def check_upgrade():
-    threading.Timer(5.0, check_upgrade).start()
-    available_product = [p for p in all_products if p not in not_available_products]
-    if len(available_product) > 0:
-        buy = available_product[-1]
-        buy_product = browser.find_element(By.ID, f'buy{buy}')
-        buy_product.click()
-
-
-timeout = time.time() + 60*5
 while True:
-    test = 0
-    if test == 5 or time.time() > timeout:
-        break
-
     cookie.click()
-    check_upgrade()
-    test = test - 1
+    if time.time() > five_sec:
+        grayed_products = browser.find_elements(By.CSS_SELECTOR, ".grayed b")
+        not_available_products = [p.text.split("-")[0].strip() for p in grayed_products]
+        available_products = [p for p in all_products if p not in not_available_products]
+        print("available products: ", available_products)
+        if len(available_products) > 0:
+            buy = available_products[-1]
+            buy_product = browser.find_element(By.ID, f'buy{buy}')
+            buy_product.click()
+            print(f'just bought {buy}')
+
+        five_sec = time.time() + 5
+
+    if time.time() > five_min:
+        cookie_per_sec = browser.find_element(By.ID, "cps").text
+        print(cookie_per_sec)
+        break
