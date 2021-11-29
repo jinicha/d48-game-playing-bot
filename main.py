5e9687792905b0fc7d0a8aca7e48d9e6af54840f
jinicha <jinichaa@gmail.com>
2021-11-28 22:59:36 -0800

    Scrape event date and title, and store them in a dictionary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import config
 
 chrome_driver_path = config.chrome_driver_path
@@ -7,4 +8,18 @@ chrome_driver_path = config.chrome_driver_path
 service = Service(chrome_driver_path)
 browser = webdriver.Chrome(service=service)
 
-browser.get("https://www.google.com")
+browser.get("https://www.python.org/")
+response = browser.find_elements(By.CSS_SELECTOR, ".event-widget time")
+events = response[0].text.split("\n")
+events_time = [e for e in events if events.index(e) % 2 == 0]
+events_title = [e for e in events if events.index(e) % 2 != 0]
+events_dict = {}
+for index, time in enumerate(events_time):
+    events_dict[index] = {
+        "time": time,
+        "name": events_title[index]
+    }
+
+print(events_dict)
+
+browser.quit()
